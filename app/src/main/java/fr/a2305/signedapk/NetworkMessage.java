c2a23aeee1d8e7bd2f8bd730ec3a9dfc8bd984d6
hagui <hagguizied7@gmail.com>
2016-07-04 16:49:03 +0200

    add network message

--- app/src/main/java/fr/a2305/signedapk/NetworkMessage.java
+++ app/src/main/java/fr/a2305/signedapk/NetworkMessage.java
@@ -5,7 +5,38 @@ package fr.a2305.signedapk;
  * Created by hagui on 2016.
  */
 public class NetworkMessage {
+    private static NetworkMessage i;
+    private boolean g;
+    private boolean f;
+    private Runnable h;
+    private NetworkMessage.CastMethod a;
+    private NetworkMessage.Protocole b;
+    private int c;
+    private byte[] d;
 
 
+    public NetworkMessage(){
+    }
 
+    private static synchronized NetworkMessage b(){
+        return i == null?(i = new NetworkMessage()):(!i.g:new NetworkMessage());
+    }
+
+
+    public enum CastMethod {
+        ALL,
+        UNICAST;
+
+        private CastMethod(){
+        }
+    }
+    public static enum Protocole {
+        TCP,
+        UDP,
+        MULTITASK;
+
+        private Protocole(){
+        }
+
+    }
 }
